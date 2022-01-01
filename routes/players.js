1191397040db4b7cedc83a1561259c26e4f7166c
Santiago <santiagoibarzabal91@gmail.com>
2022-01-01 19:57:21 -0300

    feat: move middleware to middleware file

--- routes/players.js
+++ routes/players.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
+const upload = require('../middlewares/multerMiddleware.js');
+
 
 const playersController = require('../controllers/playerController');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../public/images'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
-    }
-});
-
-const upload = multer({storage});
-
 router.get('/', playersController.index); 
-router.get('/:id/edit', playersController.edit); 
 router.get('/:id', playersController.show); 
-router.put('/:id', upload.single('avatar') ,playersController.update); 
+router.get('/:id/edit', playersController.edit); 
+router.put('/:id', upload.single('avatar'), playersController.update); 
 router.delete('/:id', playersController.delete); 
 
 module.exports = router;
